64a9f12a3aa303f5a8c57a36e66eaac928ca00ad
sydbang <sydney.bang94@gmail.com>
2022-01-10 16:44:09 -0500

    Initial Commit

---
+++ RecipeSNS/RecipeSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSNSApp.swift
+//  RecipeSNS
+//
+//  Created by Sunghee Bang on 2022-01-10.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
