c63fa19ad64d7c36683bb095b49b55fe5fd8586f
sydbang <sydney.bang94@gmail.com>
2022-06-13 18:26:22 -0400

    create user model

---
+++ RecipeSNS/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  RecipeSNS
+//
+//  Created by Sunghee Bang on 2022-06-13.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    
+    var id: String
+    var email: String
+    var name: String
+}
