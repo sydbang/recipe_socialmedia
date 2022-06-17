a71978272504d32e3d45285ab0296a0b61f59cf3
sydbang <sydney.bang94@gmail.com>
2022-06-16 23:54:29 -0400

    Update RecipeDetailView.swift
    - swap tab view
    - hide navigation bar

--- RecipeSNS/Views/Detail View/RecipeDetailView.swift
+++ RecipeSNS/Views/Detail View/RecipeDetailView.swift
@@ -12,25 +12,28 @@ struct RecipeDetailView: View {
     @State private var selectedTab: Int = 0
     
     let tabs: [Tab] = [
-        .init(icon: Image(systemName: "book.fill"), title: "directions"),
-        .init(icon: Image(systemName: "bag.fill"), title: "ingredients")
+        .init(icon: Image(systemName: "bag.fill"), title: "ingredients"),
+        .init(icon: Image(systemName: "book.fill"), title: "directions")
     ]
     
     var body: some View {
         NavigationView {
             GeometryReader { geo in
-                VStack {
+                VStack (alignment: .leading){
                     Tabs(tabs: tabs, geoWidth: geo.size.width, selectedTab: $selectedTab)
                     
                     TabView(selection: $selectedTab) {
-                        DirectionsListView()
-                            .tag(0)
+                        
                         IngredientListView()
+                            .tag(0)
+                        DirectionsListView()
                             .tag(1)
+                        
                     }
                     .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                 }
             }
+            .navigationBarHidden(true)
         }
     }
 }
